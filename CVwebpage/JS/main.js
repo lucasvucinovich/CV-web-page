9c498ca96a48972ce95cc66f2db3aad524c46ae7
lucas Agustin Vucinovich <lucasvucinovich@gmail.com>
2026-01-26 16:31:44 +0100

    Delete CVwebpage directory

--- CVwebpage/JS/main.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll(".card");
-
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    console.log("Clicked:", card.textContent.trim());
-  });
-});
